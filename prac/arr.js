f1bf3a0ac456609fbb862e6afa609cfa5858d269
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-08 16:54:41 +0000

    arr1

---
+++ prac/arr.js
@@ -0,0 +1,22 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, "Suman", "Amta"]
+const myArr2 = ["Shaktiman "]
+console.log(myArr[0]);
+
+//Array Method
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+
+const newArr = myArr.join()
+
+
+console.log(myArr)
+console.log(typeof newArr);
