9790be3fe886350aa6fc950742b36f4f9b07bc06
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-14 13:20:26 +0000

    done

---
+++ loop/sc.js
@@ -0,0 +1,7 @@
+let myArray = ["flash", "Batman", "superman"]
+
+for (let index = 0; index <= myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+    
+}
--- loop/tr.js
+++ loop/tr.js
@@ -8,4 +8,29 @@ let val1;
 //ternary operator
 
 const iceTeaPrice = 100
-iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
+// iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+// for loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element)
+}
+
+for(let i = 0; i<= 10; i++){
+    // console.log(`Outer loop value: ${i}`)
+    for(let j=0;j <= 10; j++){
+        // console.log(`Inner loop value ${j} and inner loop ${i}`)
+        console.log(i + '*' + j + ' = ' + i*j)
+    }
+}
+
+
+let myArray = ["flash", "Batman", "superman"]
+
+for (let index = 0; index <= array.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+    
+}
