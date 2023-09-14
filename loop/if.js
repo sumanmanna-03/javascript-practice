cd4382bdaa6ad320940931bead3905df6c886a57
Suman Manna <141180220+sumanmanna-03@users.noreply.github.com>
2023-09-14 12:42:21 +0000

    done

---
+++ loop/if.js
@@ -0,0 +1,34 @@
+// if 
+
+// nested if
+
+// switch case
+
+const month = 8
+switch(month){
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")  // shift + alt + down arrow
+        break;
+    case 6:
+        console.log("June")
+        break;
+     default:
+        console.log("Not in here");
+        
+}
+
+
+
+
---
+++ loop/tr.js
@@ -0,0 +1,11 @@
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// var1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+// console.log(val1)
+
+//ternary operator
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
